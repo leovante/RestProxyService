f48d812a6ad083d508aef835439a46282640f544
Andrey Rulin <andrei_rulin@epam.com>
2021-09-04 12:01:24 +0300

    Set teams routes

--- src/main/java/ru/vtb/stub/config/ZuulProxyConfig.java
+++ src/main/java/ru/vtb/stub/config/ZuulProxyConfig.java
@@ -1,20 +1,42 @@
 package ru.vtb.stub.config;
 
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.web.ServerProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.serviceregistry.Registration;
 import org.springframework.cloud.netflix.zuul.RoutesRefreshedEvent;
 import org.springframework.cloud.netflix.zuul.filters.ZuulProperties;
 import org.springframework.cloud.netflix.zuul.filters.discovery.DiscoveryClientRouteLocator;
 import org.springframework.cloud.netflix.zuul.filters.discovery.ServiceRouteMapper;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import ru.vtb.stub.filter.HostFilter;
 
+import javax.annotation.PostConstruct;
+import java.util.HashMap;
+import java.util.List;
+
+import static org.springframework.cloud.netflix.zuul.filters.ZuulProperties.ZuulRoute;
+
+@Slf4j
+@Setter
 @Configuration
+@ConfigurationProperties(prefix = "team")
 public class ZuulProxyConfig {
+
+    @Autowired
+    private ZuulProperties zuulProperties;
+    @Autowired
+    private RoutesRefreshedEvent routesRefreshedEvent;
+    @Autowired
+    public ApplicationEventPublisher publisher;
+
+    private List<String> prefix;
 
     @Bean
     public HostFilter simpleFilter() {
@@ -36,5 +58,34 @@ public class ZuulProxyConfig {
     @Bean
     public RoutesRefreshedEvent routesRefreshedEvent(@Qualifier("discoveryClientRouteLocator") DiscoveryClientRouteLocator locator) {
         return new RoutesRefreshedEvent(locator);
+    }
+
+    @PostConstruct
+    public void setTeamRoutes() {
+        if (zuulProperties.getRoutes().isEmpty()) {
+            log.info("Zuul proxy config. No default routes");
+            return;
+        }
+
+        if (prefix == null || prefix.isEmpty()) {
+            log.info("Zuul proxy config. No teams prefixes. Default routes:");
+            zuulProperties.getRoutes().forEach((k, v) -> log.info("{} --> {}", k, v));
+            return;
+        }
+
+        var routes = new HashMap<>(zuulProperties.getRoutes());
+        prefix.forEach(p -> routes.forEach((k, r) -> {
+            ZuulRoute route = new ZuulRoute();
+            route.setId(p + "-" + k);
+            route.setPath("/" + p + r.getPath());
+            route.setUrl(r.getUrl());
+            zuulProperties.getRoutes().put(route.getId(), route);
+        }));
+
+        routes.keySet().forEach(k -> zuulProperties.getRoutes().remove(k)); // delete default routes
+
+        publisher.publishEvent(routesRefreshedEvent);
+        log.info("Zuul proxy config. Teams routes:");
+        zuulProperties.getRoutes().forEach((k, v) -> log.info("{} --> {}", k, v));
     }
 }

a680d06ab856749c3bc793f9b3d0755e33fd3faa
Andrey Dorovskikh VTB4038284 <ADorovskikh@inno.tech>
2021-11-18 17:43:48 +0300

    Add new regex

--- src/main/java/ru/vtb/stub/validate/Team.java
+++ src/main/java/ru/vtb/stub/validate/Team.java
@@ -5,7 +5,7 @@ import javax.validation.Payload;
 import javax.validation.constraints.Pattern;
 import java.lang.annotation.*;
 
-@Pattern(regexp = "^[a-z0-9_-]+$")
+@Pattern(regexp = "^[a-zA-Z0-9.@/_-]+$")
 
 @Target({ElementType.METHOD, ElementType.FIELD, ElementType.PARAMETER})
 @Retention(RetentionPolicy.RUNTIME)

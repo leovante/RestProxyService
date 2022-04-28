adf22f8fa1574361acc83e620ca67a6738d09748
Andrey Rulin <ARulin@inno.tech>
2022-04-28 22:48:51 +0300

    Add validate responses

--- src/main/java/ru/vtb/stub/domain/StubData.java
+++ src/main/java/ru/vtb/stub/domain/StubData.java
@@ -44,6 +44,10 @@ public class StubData {
     @Schema(description = "Параметры ответа")
     private Response response;
 
+    @Schema(accessMode = Schema.AccessMode.READ_ONLY)
     private int count;
+
+    @Valid
+    @Schema(description = "Список параметров ответа")
     private List<Response> responses;
 }

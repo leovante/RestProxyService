0522fc61421795c407d1a1f8159d1cbc4fde37d8
Andrey Rulin <ARulin@inno.tech>
2023-05-25 21:03:37 +0300

    Response produces application/json, text/plain

--- src/main/java/ru/vtb/stub/controller/ResponseController.java
+++ src/main/java/ru/vtb/stub/controller/ResponseController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 import ru.vtb.stub.filter.RequestWrapper;
 import ru.vtb.stub.service.ResponseService;
 
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+import static org.springframework.http.MediaType.TEXT_PLAIN_VALUE;
 import static org.springframework.web.bind.annotation.RequestMethod.*;
 
 @RestController
@@ -18,7 +20,8 @@ public class ResponseController {
     private ResponseService service;
 
     @Operation(hidden = true)
-    @RequestMapping(path = "${path.response}", method = {GET, POST, PUT, PATCH, DELETE})
+    @RequestMapping(path = "${path.response}", method = {GET, POST, PUT, PATCH, DELETE},
+            produces = {APPLICATION_JSON_VALUE,  TEXT_PLAIN_VALUE})
     public ResponseEntity<Object> response(
             @RequestParam String rpsRequest,
             @RequestParam String rpsKey,

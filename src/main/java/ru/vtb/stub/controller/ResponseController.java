9a24f034bf2b5d60c4ec1484512c6bb0e9a771dd
Кирдеева Екатерина Витальевна (4091033) <kirdeeva@vtb.ru>
2023-09-05 23:16:29 +0300

    ONLINELK-4317 add more info into log for HttpMediaTypeNotAcceptableException

--- src/main/java/ru/vtb/stub/controller/ResponseController.java
+++ src/main/java/ru/vtb/stub/controller/ResponseController.java
@@ -1,18 +1,27 @@
 package ru.vtb.stub.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpMediaTypeNotAcceptableException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import ru.vtb.stub.filter.RequestWrapper;
 import ru.vtb.stub.service.ResponseService;
+
+import javax.servlet.http.HttpServletRequest;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 import static org.springframework.http.MediaType.TEXT_PLAIN_VALUE;
 import static org.springframework.web.bind.annotation.RequestMethod.*;
 
+@Slf4j
 @RestController
 public class ResponseController {
 
@@ -28,5 +37,13 @@ public class ResponseController {
             RequestWrapper servletRequest
     ) {
         return service.sendResponse(rpsRequest, rpsKey, servletRequest);
+    }
+
+    @ResponseBody
+    @ExceptionHandler(HttpMediaTypeNotAcceptableException.class)
+    public void handleHttpMediaTypeNotAcceptableException(HttpMediaTypeNotAcceptableException e,
+                                                            HttpServletRequest request) {
+        log.error("No acceptable representation found for [{}] | supported {}", request.getHeader("Accept"),
+                e.getSupportedMediaTypes());
     }
 }

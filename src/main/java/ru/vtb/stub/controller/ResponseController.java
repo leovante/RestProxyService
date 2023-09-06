5934671989c4483c44022a27b258e120d9d87b0e
Кирдеева Екатерина Витальевна (4091033) <kirdeeva@vtb.ru>
2023-09-06 13:23:30 +0300

    ONLINELK-4317 reduce accepted headers range

--- src/main/java/ru/vtb/stub/controller/ResponseController.java
+++ src/main/java/ru/vtb/stub/controller/ResponseController.java
@@ -3,8 +3,6 @@ package ru.vtb.stub.controller;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.HttpMediaTypeNotAcceptableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -31,7 +29,7 @@ public class ResponseController {
 
     @Operation(hidden = true)
     @RequestMapping(path = "${path.response}", method = {GET, POST, PUT, PATCH, DELETE},
-            produces = {ALL_VALUE})
+            produces = {APPLICATION_JSON_VALUE,  TEXT_PLAIN_VALUE, "application/vnd.schemaregistry.v1+json"})
     public ResponseEntity<Object> response(
             @RequestParam String rpsRequest,
             @RequestParam String rpsKey,
@@ -40,12 +38,12 @@ public class ResponseController {
         return service.sendResponse(rpsRequest, rpsKey, servletRequest);
     }
 
-//    @ResponseBody
-//    @ExceptionHandler(HttpMediaTypeNotAcceptableException.class)
-//    public ResponseEntity<Object> handleHttpMediaTypeNotAcceptableException(HttpMediaTypeNotAcceptableException e,
-//                                                            HttpServletRequest request) {
-//        log.error("No acceptable representation found for [{}] | supported {}", request.getHeader("Accept"),
-//                e.getSupportedMediaTypes());
-//        return ResponseEntity.badRequest().build();
-//    }
+    @ResponseBody
+    @ExceptionHandler(HttpMediaTypeNotAcceptableException.class)
+    public ResponseEntity<Object> handleHttpMediaTypeNotAcceptableException(HttpMediaTypeNotAcceptableException e,
+                                                            HttpServletRequest request) {
+        log.error("No acceptable representation found for [{}] | supported {}", request.getHeader("Accept"),
+                e.getSupportedMediaTypes());
+        return ResponseEntity.badRequest().build();
+    }
 }
